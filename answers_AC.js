58648b1230e21e986e6d1a4661c8bd518f29e941
Autociudad <Autociudad@users.noreply.github.com>
2017-03-20 14:57:33 -0300

    Update answers_AC.js

--- answers_AC.js
+++ answers_AC.js
@@ -56,11 +56,13 @@ htmlContent += '<div id="myNav" class="overlay">';
 htmlContent += '  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>';
 htmlContent += '  <div class="overlay-content">';
 htmlContent += '    <a href="javascript:updateAnswer(\'Hola, en el catálogo del fabricante nos figura que este es el producto que aplica a tu vehículo. Tenemos el producto en stock. Esperamos tu oferta. Saludos Autociudad\');">Es el producto y stock</a>';
-htmlContent += '    <a href="javascript:updateAnswer(\'Hola, tenemos el producto en stock. Esperamos tu oferta. Saludos Autociudad\');">En Stock</a>';
 htmlContent += '    <a href="javascript:updateAnswer(\'Hola, el producto es nuevo con garantía de fabrica por desperfectos de fabricacion. Esperamos tu oferta. Saludos Autociudad\');">Producto Nuevo</a>';
+htmlContent += '    <a href="javascript:updateAnswer(\'Hola, tenemos el producto en stock. Esperamos tu oferta. Saludos Autociudad\');">En Stock</a>';
+htmlContent += '    <a href="javascript:updateAnswer(\'Hola, no tenemos disponible el artículo que andas necesitando. Esperamos poder ayudarte la próxima. Saludos Autociudad.\');">No tenemos el artículo</a>';
 htmlContent += '    <a href="javascript:updateAnswer(\'Hola, para estar 100% seguros, te pido los codigos que figuran en tu arranque. Gracias! Saludos Autociudad.\');">Nro de Pieza STR</a>';
 htmlContent += '    <a href="javascript:updateAnswer(\'Hola, para estar 100% seguros, te pido los codigos que figuran en tu alternador. Gracias! Saludos Autociudad.\');">Nro de Pieza ALT</a>';
-htmlContent += '    <a href="javascript:updateAnswer(\'Hola, Mercadolibre sólo acepta pagos vía Mercadopago (tarjeta de crédito o rapipago/pagofacil). Gracias! Saludos Autociudad.\');">Sólo MP</a>';
+htmlContent += '    <a href="javascript:updateAnswer(\'Hola, para estar 100% seguros, te pido nos pases los 17 digitos del nro de chasis (figura en la cedula verde). Gracias! Saludos Autociudad.\');">Nro de Chasis</a>';
+htmlContent += '    <a href="javascript:updateAnswer(\'Hola, desde hace un tiempo MercadoLibre sólo permite abonar utilizando MercadoPago (esto es con tarjeta o en efectivo vía Pago Fácil o similar). Saludos Autociudad\');">Formas de pago</a>';
 htmlContent += '    <a href="javascript:updateAnswer(\'Podes retirarlo en persona por nuestro local en el barrio de mataderos o bien pedir envío por OCA. Saludos Autociudad.\');">Retiro / Envio</a>';
 htmlContent += '    <a href="#"><hr></a>';
 htmlContent += '    <a href="javascript:updateAnswer(\'La dirección de retiro es Albariño 1578 - CABA (altura Av. Directorio al 5300) - Mataderos - De Lunes a Viernes de 8:00 a 12:00 y de 14:00 a 18:00. Saludos Autociudad.\');">Dirección</a>';
@@ -96,5 +98,13 @@ htmlContent += '}';
 htmlContent += 'openNav();'
 
 htmlContent += '</script>';
+
+/*
+$("p[data-js='question-row-text']").html().trim() <-- obtiene el texto de la pregunta (cuando hay una sola)
+$("textarea[data-js='question-replay__textarea']").val() <-- obtiene el texto del text area de la respuesta (cuando hay una sola)
+
+$("textarea[data-js='question-replay__textarea']").val("setea el valor") <-- setea el texto del text area de la respuesta (cuando hay una sola)
+$("textarea[data-js='question-replay__textarea']:eq(0)").val("que miras gil??") <-- setea el texto del text area de la respuesta (opción 0 de las N posibles)
+*/
 
 $("body").append(htmlContent);
